46d4ca99a08c30add274897b7f7768b58b86cf0c
shunsuke-tamura <wwwxx4869@gmail.com>
2022-12-18 03:26:27 +0900

    update: resultにprops実装

--- fe/src/routes/result.tsx
+++ fe/src/routes/result.tsx
@@ -1,23 +1,38 @@
-import Side_Button from "~/components/side_Button";
+type Props = {
+  angle: number;
+  score: number;
+  captureUrl: string;
+};
 
-export default function Result() {
+export default function Result(props: Props) {
+  const commonClasses = [
+    "bg-btn_blue",
+    "font-semibold",
+    "text-font_blue",
+    "text-xl",
+    "py-6",
+    "px-10",
+    "absolute",
+    "right-0",
+    "z-10",
+  ];
+  const resultWindowClasses = [
+    ...commonClasses,
+    "text-center",
+    "top-0",
+    "rounded-bl-2xl",
+  ];
+  const returnButtonClasses = [...commonClasses, "rounded-tl-2xl", "bottom-0"];
   return (
     <>
       <main class="mx-auto w-full h-full bg-white">
-        <a
-          class={`bg-btn_blue font-semibold text-font_blue text-center text-xl
-      py-6 px-10 rounded-bl-2xl absolute top-0 right-0 z-10`}
-        >
-          30°
+        <img src={props.captureUrl}></img>
+        <a class={`${resultWindowClasses.join(" ")}`}>
+          {props.angle}°
           <br />
-          誤差:0.064°
+          誤差:{props.score}°
         </a>
-
-        <a
-          class={`bg-btn_blue font-semibold text-font_blue text-xl
-      py-6 px-10 rounded-tl-2xl absolute bottom-0 right-0 z-10`}
-          href="/"
-        >
+        <a class={`${returnButtonClasses.join(" ")}`} href="/">
           <button class="w-full h-full">戻る</button>
         </a>
       </main>
