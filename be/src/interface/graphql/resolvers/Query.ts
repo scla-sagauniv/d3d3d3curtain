f450922ca4d115b26a03e3e568f961ee6732551c
nnnnn81 <60097331+nnnnn81@users.noreply.github.com>
2022-12-17 08:06:38 +0000

    add: angleの値を返すクエリを追加

--- be/src/interface/graphql/resolvers/Query.ts
+++ be/src/interface/graphql/resolvers/Query.ts
@@ -1,6 +1,8 @@
 import {QueryResolvers} from '../generated/graphql';
 import greet from '../../db/store';
+import {CreateTarget} from '../../lib/Target';
 
 export const Query: QueryResolvers = {
   greet: () => greet.getGreet(),
+  target: () => CreateTarget(),
 };
